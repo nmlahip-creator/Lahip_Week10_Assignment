2ada336fb8b6d047d93caaaabff3e4ff5e33c792
nmlahip-creator <nmlahip@ucdavis.edu>
2026-03-15 15:12:53 -0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Week 10 Assignment", page_icon="💬")
+
+st.title("Week 10 Assignment")
+st.write("This is a placeholder UI for Task 1. It will be replaced with the final app.")
